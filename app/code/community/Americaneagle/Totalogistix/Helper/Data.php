6ea4670d496c74301e15f8111d8239ea23b7d8d5
Andrew Stayart <andrew.stayart@americaneagle.com>
2014-09-24 16:47:07 -0500

    Initial structure for Totalogistix shipping module

---
+++ app/code/community/Americaneagle/Totalogistix/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: magentouser
+ * Date: 9/24/14
+ * Time: 4:05 PM
+ */
+
+class Americaneagle_Totalogistix_Helper_Data extends Mage_Core_Helper_Abstract {
+
+} 
--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: magentouser
- * Date: 9/18/14
- * Time: 11:16 AM
- */ 
