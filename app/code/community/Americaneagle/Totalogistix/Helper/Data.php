73c91a2ea15489e6e5331b79b6608ba691e57211
Andrew Stayart <astayart@gmail.com>
2014-10-02 16:42:17 -0500

    initial version of TLX integration

--- app/code/community/Americaneagle/Totalogistix/Helper/Data.php
+++ app/code/community/Americaneagle/Totalogistix/Helper/Data.php
@@ -1,11 +1,89 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: magentouser
  * Date: 9/24/14
  * Time: 4:05 PM
  */
-
 class Americaneagle_Totalogistix_Helper_Data extends Mage_Core_Helper_Abstract {
+	private $req;
+	public function getPriceSheets(Mage_Shipping_Model_Rate_Request $request) {
 
-} 
+		$this->req = $request;
+		$xItems = new SimpleXMLElement('<Items></Items>');
+		foreach($request->getAllItems() as $item) {
+			if($item->getProductType() == 'simple'){
+				$xitem = $xItems->addChild('Item');
+				$xitem->addChild('Class', $item->getProduct()->load($item->getProduct()->getId())->getClass());
+				$xitem->addChild('Weight', intval($item->getWeight()));
+			}
+		}
+
+		$client = new Zend_Http_Client();
+		$client->setUri($this->getServiceUri());
+		$client->setParameterPost('szip', $this->getOriginZip());
+		$client->setParameterPost('AccessID', $this->getAccessId());
+		$client->setParameterPost('service', $this->getAccessorial());
+		$client->setParameterPost('date', $this->getShipDate());
+		$client->setParameterPost('czip', $request->getDestPostcode());
+		$client->setParameterPost('items', $xItems->asXML());
+
+		Mage::log("TOTALogistix: Posting Accessorial: " . $this->getAccessorial());
+		Mage::log("TOTALogistix: Posting XML: " . $xItems->asXML());
+
+		$response = $client->request('POST');
+		$xml = simplexml_load_string($response->getBody());
+		$status = $xml->xpath('/Response')[0]->{"Status"};
+		Mage::log('TOTALogistix: received status: ' . $status->asXML());
+		if($status === false) {
+			Mage::logException("Failed to load response from TOTALogistix");
+		}
+		$prices = $xml->xpath('/Response/PriceSheet');
+		if($prices === false) {
+			Mage::logException("Failed to load shipping price sheets");
+		}
+		$sheets = array();
+		foreach($prices as $price){
+			$ps = new Varien_Object();
+
+			$ps->setCarrier((string) $price->{'Carrier'});
+			$ps->setChargeAmount((string) $price->{'ChargeAmount'});
+			$ps->setCarrierName((string) $price->{'CarrierName'});
+			$sheets[] = $ps;
+		}
+
+		/* just for testing, add a bogus sheet: */
+//		$ps = new Varien_Object();
+//		$ps->setCarrier('foo carrier');
+//		$ps->setCarrierName('foo carrier');
+//		$ps->setChargeAmount("59.08");
+//		$sheets[] = $ps;
+
+		return $sheets;
+	}
+
+	private function getAccessorial() {
+		if($this->req->getIsResidential() == 1) {
+			return 'REP,' . Mage::getStoreConfig('carriers/totalogistix/accessorial');
+		} else {
+			return Mage::getStoreConfig('carriers/totalogistix/accessorial');
+		}
+	}
+	private function getShipDate() {
+		$lead = Mage::getStoreConfig('carriers/totalogistix/lead_time');
+		$d = new Zend_Date();
+		$d->add($lead, Zend_Date::DAY);
+		return $d->toString('MM/dd/YYYY');	//	$client->setParameterGet('date', '10/10/2014');
+	}
+	private function getAccessId() {
+		return Mage::getStoreConfig('carriers/totalogistix/access_id');
+	}
+
+	private function getOriginZip() {
+		return Mage::getStoreConfig('carriers/totalogistix/origin_zip');
+	}
+	private function getServiceUri() {
+		return Mage::getStoreConfig('carriers/totalogistix/service_url');
+	}
+}
---
+++ app/code/community/Americaneagle/Totalogistix/sql/americaneagle_totalogistix_setup/install-1.0.0.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: magentouser
+ * Date: 10/1/14
+ * Time: 3:36 PM
+ */
+/** @var Mage_Customer_Model_Entity_Setup $installer */
+$installer = $this;
+$installer->startSetup();
+$installer->addAttribute('customer_address', 'is_residential', array(
+	'type' => 'static',
+	'input' => 'boolean',
+	'label' => 'Residential Address',
+	'global' => 1
+));
+Mage::getSingleton('eav/config')
+	->getAttribute('customer_address', 'is_residential')
+	->setData('used_in_forms', array(
+		'customer_register_address',
+		'customer_address_edit',
+		'adminhtml_customer_address'
+	))
+	->save();
+$installer->endSetup();
