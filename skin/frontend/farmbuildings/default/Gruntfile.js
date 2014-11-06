0302075e168d23ee47d73b51734e699f4ca9400d
Andrew Stayart <astayart@gmail.com>
2014-11-06 09:49:34 -0600

    merged ie10 changes

--- skin/frontend/farmbuildings/default/Gruntfile.js
+++ skin/frontend/farmbuildings/default/Gruntfile.js
@@ -20,7 +20,8 @@ module.exports = function(grunt) {
           outputStyle: 'compressed'
         },
         files: {
-          'css/app.css': 'scss/app.scss'
+          'css/app.css': 'scss/app.scss',
+          'css/ie.css': 'scss/ie.scss'
         }        
       }
     },
