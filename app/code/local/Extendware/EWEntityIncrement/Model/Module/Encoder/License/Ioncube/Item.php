dadecfc1332a47960bb9e2446f569ccbc03f4309
Andrew Stayart <astayart@gmail.com>
2015-05-27 09:04:51 -0500

    git ignore cleaning

--- app/code/local/Extendware/EWEntityIncrement/Model/Module/Encoder/License/Ioncube/Item.php
+++
@@ -1,4 +0,0 @@
-<?php /*obfv1*/
-
-
-$_F=__FILE__;$_X="eJyzt7OxL8goUNDXyk9KKzPU0uflUkjLzEvMUUjOSSwuVnCtKEnNSylPLEqNdw13zSvJLKn0zEsuSs1NzSuJ981PSc0BkaU5QOm8ZCC3KN4nMzk1rzg13jM/L7k0CUiXpOYqpIKNIco4n8ykosSiynjnfKAiYsyuVqgFAAbmSk8=";$_D=strrev("edoced_46esab");eval(gzuncompress($_D($_X)));
