296a35a2dde113430173b8b4389f66120bf46d77
Andrew Stayart <andrew.stayart@americaneagle.com>
2015-11-04 11:07:25 -0600

    cleaning up master

---
+++ app/code/local/Americaneagle/Farmbuildings/Block/Checkout/Renderer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: magentouser
+ * Date: 10/29/14
+ * Time: 1:28 PM
+ */
+
+class Americaneagle_Farmbuildings_Block_Checkout_Renderer {
+
+} 
---
+++ app/code/local/Extendware/EWCore/Model/Mage/Core/Singleton/Abstract.php
@@ -0,0 +1,18 @@
+<?php /*obfv1*/
+// Copyright © 2015 Extendware
+// Are you trying to customize your extension? Contact us (http://www.extendware.com/contacts/) and we can help!
+// Please note, not all files are encoded and different extensions have different levels of encoding.
+// We are always happy to provide guideance if you are experiencing an issue!
+
+
+
+/**
+ * Below are methods found in this class
+ *
+ * @method mixed final public __clone()
+ * @method mixed public __construct()
+ * @method mixed static public singleton()
+ *
+ */
+
+$_F=__FILE__;$_X="eJy1VNtu00AQfc9XTKtKdSJUx3bupRSo+sADEoIHHgBZa+9sstJm19pdxw0VH8Rv8GXMOiRNWxABhGVZsn32zJk5M3P57Olltagg7plCrJJe3IljuDLV2sr5wsO3r5D2kyFc33jUvGEWw/8XFmFtavB2LfUcvIGydt4s5ef2uwUMcCeNviQu7VnpoXYQLbyvZnHcNM0Z7gjPSrOMyw3KxV1gmkODUDINC1TVUQj4RiFzCNp4fBKewJQCIRU6IAZAXRqOvD3KpRBoUfs7EQ4WbIV7fxSuUDkwYnOScjgLUd5jy8ZUw9bhTFWtQ26VNSvJEeY1PZkuEaRo029D31RoJdGEQpBk6VyNRx264l6vAz14ico0LXSJfmG4A2Fq0ik1+IV0UCrmHAED9vkGAkt5Q9kIqZmCqi6ULCHPS2U0Rt3HuDsEpeptXfqfoZxnnlA/wI7kKvRGt1C6487lphM6rCCS4FirbM/5V9qjJUn59fsrYzF/TTVX+Ws2x7x9f7flzF9sKTYu75P86vDuyO1OqZUr5hFO8kHWFykOxZCN+GBYpJM0ywTP0gljYpD0WZqkhUimJVxQnS1bR93zbZ6i1qWnFrhfHQpCrNMi6w+m2SBJisloPGXDcX+cjZK0PyimA+yzoeDJkCGxztHnbTWik+AZ0VMHRGQ1+sihErPZ4SI/HB75UxcuLi5AMOUwaP6foSjLNjeqHAszMpvdq9g5fAEaGSQVXGJ0/FFftd0RHcPZo/LQp+MuuIWpFQdNs2ahQFCGhRldktvU+TQqhkbpoz5uub886M+db3uN2romRqNJNh7wbFyMxXiSlkkipv2xKEdsknBG+RSTKRP/wbVDIz90TWPzB7o3pbboa6v/xvCDVYYw9zbM/qhsVs0/mN0y8Dt3t8slOMCR4iKPTu+2wuz3C+Lxdpm9xcZKj6cPCv7LBfZPi+svtt9tyPw7RvFojA==";$_D=strrev("edoced_46esab");eval(gzuncompress($_D($_X)));
