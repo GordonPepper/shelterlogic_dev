9e5057fe62f0d76635ed5f1e271f8a2f78095761
Andrew Stayart <andrew.stayart@americaneagle.com>
2015-11-13 09:29:10 -0600

    missing file

---
+++ app/code/local/Americaneagle/Farmbuildings/Block/Adminhtml/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mageuser
+ * Date: 11/11/15
+ * Time: 4:26 PM
+ */
+class Americaneagle_Farmbuildings_Block_Adminhtml_Config
+extends Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config
+{
+    public function canShowTab() {
+        if($this->getProductId() == '22926') {
+            return false;
+        }
+        return parent::canShowTab();
+    }
+}
