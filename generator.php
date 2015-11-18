cb4c4b76fbf01b43d1409eae072cf64f72c8713e
Levente Albert <levente.albert@americaneagle.com>
2015-11-18 16:37:11 -0600

    adding generator file

---
+++ generator.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$generator = new \Wsdl2PhpGenerator\Generator();
+$generator->generate(
+    new \Wsdl2PhpGenerator\Config(array(
+        'inputFile' => 'https://slvisual.shelterlogicdirect.com/derp/SalesOrderService.asmx?wsdl',
+        'outputDir' => 'app/code/community/Americaneagle/Visual/wsdl/SalesOrderService',
+        'namespaceName' => 'Visual\SalesOrderService'
+    ))
+);
+$generator->generate(
+    new \Wsdl2PhpGenerator\Config(array(
+        'inputFile' => 'https://slvisual.shelterlogicdirect.com/derp/CustomerService.asmx?wsdl',
+        'outputDir' => 'app/code/community/Americaneagle/Visual/wsdl/CustomerService',
+        'namespaceName' => 'Visual\CustomerService'
+    ))
+);
