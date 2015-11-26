33384561f8571b6b2c1e21708ed2b2329a2175e2
Andrew Stayart <andrew.stayart@americaneagle.com>
2015-11-26 14:15:46 -0600

    multi warehouse working in default setup, need to inject warehouse rules

---
+++ app/code/community/Americaneagle/Totalogistix/Model/Index/Observer.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mageuser
+ * Date: 11/25/15
+ * Time: 4:17 PM
+ */
+class Americaneagle_Totalogistix_Model_Index_Observer extends Enterprise_CatalogInventory_Model_Index_Observer
+{
+    /**
+     * @param Varien_Event_Observer $observer
+     */
+    public function reindexQuoteInventory($observer) {
+        return;
+    }
+
+}
