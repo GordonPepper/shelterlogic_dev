e3e7d941ab82c58e67e8c9f9335865c5c5770022
mageuser <gregn@americaneagle.com>
2015-12-21 12:16:44 -0600

    Mobile Footer and Additional Products collapsible

--- skin/frontend/farmbuildings/shelterlogic/js/app.js
+++ skin/frontend/farmbuildings/shelterlogic/js/app.js
@@ -811,25 +811,60 @@ $j(document).ready(function() {
     ProductMediaManager.init();
 });
 
+/*
+ * jQuery throttle / debounce - v1.1 - 3/7/2010
+ * http://benalman.com/projects/jquery-throttle-debounce-plugin/
+ *
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+(function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
+
+/* Slide Toggle */
+
 $j.fn.xMod=function(o) {
 	var sObj=this,
 	trigger=o.trigger,
 	target=o.target,
 	sObjTrig;
-	$j(trigger,sObj).on({
-		click:function(e){
-			e.preventDefault();
-			sObjTrig=$j(this);
-			sObjTrig.closest(sObj).find(target).slideToggle(400);
-		}
-	});
+	if (o.xbind) {
+        $j(trigger,sObj).on({
+            click:function(e){
+                e.preventDefault();
+                $j(this).toggleClass("active").closest(sObj).find(target).slideToggle(400);
+            }
+        });
+    } else {
+        $j(trigger,sObj).off("click");
+        $j(this).closest(sObj).find(target).removeAttr("style");
+    }
 }
 
 $j(document).ready(function() {
-	$j(".x-mod").each(function() {
-		$j(this).xMod({
-			trigger:".x-trigger",
-			target:".x-target"
-		});
-	});
+
+    function xModBind(b,o) {
+        $j(".x-mod",o).each(function() {
+            $j(this).xMod({
+                xbind:b,
+                trigger:".x-trigger",
+                target:".x-target"
+            });
+        });
+    }
+
+    xModBind(true,"body");
+
+    function spFN() {
+        if ($j("#mobitoggle").is(":visible")) {
+            xModBind(true,"#footer");
+        } else {
+            xModBind(false,"#footer");
+        }
+    }
+
+    spFN();
+
+    $j(window).resize($j.debounce(500,spFN));
+
 });
