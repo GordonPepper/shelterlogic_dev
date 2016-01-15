9fb1090fd3eeb0621c73eac69ed7eb177e32c65c
Levente Albert <levente.albert@americaneagle.com>
2016-01-15 09:44:50 -0600

    fixing upgrade scripts for visual

---
+++ app/code/community/Americaneagle/Visual/sql/americaneagle_visual_setup/upgrade-1.0.1-1.0.2.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * upgrade script for removing address attributes and adding a couple customer attributes
+ *
+ * @author Levente Albert
+ * @since 1/15/16
+ */
+
+$installer = $this;
+$installer->startSetup();
+
+$installer->removeAttribute('customer_address','is_residential');
+$installer->removeAttribute('customer_address','is_limited_access');
+
+$installer->addAttribute('customer', 'visual_customer_id', array(
+    'type' => 'text',
+    'input' => 'text',
+    'label' => 'Visual Customer ID',
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'default' => ''
+));
+
+$installer->addAttribute('customer', 'credit_status', array(
+    'type' => 'text',
+    'input' => 'text',
+    'label' => 'Credit Status',
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'default' => ''
+));
+
+$installer->addAttribute('customer', 'price_group', array(
+    'type' => 'text',
+    'input' => 'text',
+    'label' => 'Price Group',
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'default' => ''
+));
+
+$installer->addAttribute('customer', 'discount_percent', array(
+    'type' => 'text',
+    'input' => 'text',
+    'label' => 'Discount Percent',
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'default' => ''
+));
+
+$installer->addAttribute('customer', 'phone', array(
+    'type' => 'text',
+    'input' => 'text',
+    'label' => 'Phone',
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'default' => '',
+    'required' => true,
+    'visible' => true
+));
+
+$installer->endSetup();
---
+++ app/code/community/Americaneagle/Visual/sql/americaneagle_visual_setup/upgrade-1.0.2-1.0.3.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * upgrade script for adding a terms id to customer
+ *
+ * @author Levente Albert
+ * @since 1/15/16
+ */
+
+$installer = $this;
+$installer->startSetup();
+
+$installer->addAttribute('customer', 'terms_id', array(
+    'type' => 'text',
+    'input' => 'text',
+    'label' => 'Terms ID',
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'default' => ''
+));
+
+$installer->endSetup();
