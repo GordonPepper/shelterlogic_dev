7f32c7610659c5db7f26ffae3f9c24e4ab089bc4
Levente Albert <levente.albert@americaneagle.com>
2016-01-26 13:19:33 -0600

    adding start number to the order number

--- app/code/local/Shelterlogic/Orderprefix/Model/Eav/Entity/Type.php
+++ app/code/local/Shelterlogic/Orderprefix/Model/Eav/Entity/Type.php
@@ -2,6 +2,7 @@
 class Shelterlogic_Orderprefix_Model_Eav_Entity_Type extends Mage_Eav_Model_Entity_Type
 {
     const XML_PATH_ORDER_PREFIX = 'shelterlogic/orderprefix/order';
+    const XML_PATH_ORDER_START_NUMBER = 'shelterlogic/orderprefix/startnumber';
 
     protected $_allowEntities = array('order');
 
@@ -10,6 +11,17 @@ class Shelterlogic_Orderprefix_Model_Eav_Entity_Type extends Mage_Eav_Model_Enti
         $originalIncrementId = parent::fetchNewIncrementId($storeId);
 
         if (in_array($this->getEntityTypeCode(), $this->_allowEntities)) {
+            if ($originalIncrementId) {
+                $startNumber = Mage::getStoreConfig(self::XML_PATH_ORDER_START_NUMBER, $storeId);
+                if ($startNumber > $originalIncrementId) {
+                    $entityStoreConfig = Mage::getModel('eav/entity_store')
+                        ->loadByEntityStore($this->getId(), $storeId);
+                    $entityStoreConfig->setIncrementLastId($startNumber);
+                    $entityStoreConfig->save();
+                    $originalIncrementId = $startNumber;
+                }
+            }
+
             $originalIncrementId = Mage::getStoreConfig(self::XML_PATH_ORDER_PREFIX, $storeId) . $originalIncrementId;
         }
 
