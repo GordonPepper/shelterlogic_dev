526db7e4189b33b6ce8cc82b38cae6a61f412186
Bryan Bielefeldt <bryan@typoglyphic.org>
2016-09-19 12:05:10 -0400

    test console on Query parameters

---
+++ skin/frontend/farmbuildings/shelterlogic/js/configurable.querylistener.js
@@ -0,0 +1,28 @@
+/// Avoid PrototypeJS conflicts, assign jQuery to $j instead of $
+var $c = jQuery.noConflict();
+
+// Use $j(document).ready() because Magento executes Prototype inline
+$c(document).ready(function() {
+	/*
+	|--------------------------------------------------------------------------
+	| query params listener
+	|--------------------------------------------------------------------------
+	*/
+	function getconfigVars(qp){
+	    var vars = [], hash;
+	    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	    for(var i = 0; i < hashes.length; i++)
+	    {
+	        hash = hashes[i].split('=');
+	        if(hash[0].startsWith(qp)) {
+	        	vars.push(hash[0]);
+	        	vars[hash[0]] = hash[1];
+	        }
+	    }
+	    return vars;
+	}
+	$.each(getconfigVars("configurable_"), function(k, v) {
+    	console.log(k + " , " + v);
+  	});
+
+});
